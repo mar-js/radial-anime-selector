c4b8d769b9ce96aca1d98f3a95e87b13c6cac64c
Martin <maguera37@gmail.com>
2026-02-28 23:08:39 -0300

    feat: added character data for Goku, Naruto, Luffy, and Ichigo to the data mockup

---
+++ src/mocks/index.js
@@ -0,0 +1,22 @@
+export const DATA = [
+  {
+    id: 1,
+    name: 'Goku',
+    image: '/goku.png',
+  },
+  {
+    id: 2,
+    name: 'Naruto',
+    image: '/naruto.png',
+  },
+  {
+    id: 3,
+    name: 'Luffy',
+    image: '/luffy.png',
+  },
+  {
+    id: 4,
+    name: 'Ichigo',
+    image: '/ichigo.png',
+  },
+];
