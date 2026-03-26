c4187a24188294c7666a98ab8fb54eacd3339f7a
Martin <maguera37@gmail.com>
2026-03-26 02:59:20 -0300

    feat: simplified the DeleteButton component with improved styles

--- src/components/delete-button.jsx
+++ src/components/delete-button.jsx
@@ -1,13 +1,5 @@
 export const DeleteButton = ({ onClick }) => (
-  <button
-    onClick={onClick}
-    className="pointer inline-block m-5 border-2 border-transparent"
-    type="button"
-  >
-    <img
-      src="/icons/delete.svg"
-      alt="Delete"
-      className="w-[100px] h-[100px] object-fill rounded-[50px]"
-    />
+  <button onClick={onClick} className="icon-button delete-button" type="button">
+    <img src="/icons/delete.svg" alt="Delete" className="icon-image-button" />
   </button>
 );
