6fe5be7ce11516235e91d894127425c8b6cf348a
Martin <maguera37@gmail.com>
2026-03-26 02:59:28 -0300

    feat: refactoed EditButton component for improved styling

--- src/components/edit-button.jsx
+++ src/components/edit-button.jsx
@@ -1,13 +1,5 @@
 export const EditButton = ({ onClick }) => (
-  <button
-    onClick={onClick}
-    className="pointer inline-block m-5 border-2 border-transparent"
-    type="button"
-  >
-    <img
-      src="/icons/edit.svg"
-      alt="Edit"
-      className="w-[100px] h-[100px] object-fill rounded-[50px]"
-    />
+  <button onClick={onClick} className="icon-button edit-button" type="button">
+    <img src="/icons/edit.svg" alt="Edit" className="icon-image-button" />
   </button>
 );
