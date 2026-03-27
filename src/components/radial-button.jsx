72b14fd1a6b22b013de8add9cc108e6e3912e20a
Martin <maguera37@gmail.com>
2026-03-27 15:12:57 -0300

    feat: refactored RadialButton component for improved structure and styling

--- src/components/radial-button.jsx
+++ src/components/radial-button.jsx
@@ -1,15 +1,16 @@
 export const RadialButton = ({ id, name, image }) => (
-  <>
-    <input type="radio" id={`opcion-${id}`} name="opcion" className="hidden" />
+  <div className="radial-button">
+    <input
+      type="radio"
+      id={`opcion-${id}`}
+      name="opcion"
+      className="radial-input"
+    />
     <label
       htmlFor={`opcion-${id}`}
-      className="pointer inline-block m-5 border-2 border-transparent"
+      className="radial-label pointer inline-block m-5 border-2 border-transparent"
     >
-      <img
-        src={image}
-        alt={name}
-        className="w-[100px] h-[100px] object-fill rounded-[50px]"
-      />
+      <img src={image} alt={name} />
     </label>
-  </>
+  </div>
 );
