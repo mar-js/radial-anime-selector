b8ad894a1cdeb6f8974629a6637cca863c7d867f
Martin <maguera37@gmail.com>
2026-03-31 16:56:56 -0300

    feat: refactored RadialButton to use context for active state management

--- src/components/radial-button.jsx
+++ src/components/radial-button.jsx
@@ -1,19 +1,30 @@
-export const RadialButton = ({ id, name, image }) => (
-  <div
-    className={`radial-button ${name === 'Goku' ? 'active' : ''}`}
-    ariaLabel={name}
-  >
-    <input
-      type="radio"
-      id={`opcion-${id}`}
-      name="opcion"
-      className="radial-input"
-    />
-    <label
-      htmlFor={`opcion-${id}`}
-      className="radial-label pointer inline-block m-5 border-2 border-transparent"
+import { useData } from '../contexts';
+
+export const RadialButton = ({ id, name, image }) => {
+  const { data, handleData } = useData();
+
+  const handleClick = () => {
+    handleData(name);
+  };
+
+  return (
+    <div
+      className={`radial-button ${name === data ? 'active' : ''}`}
+      ariaLabel={name}
     >
-      <img src={image} alt={name} />
-    </label>
-  </div>
-);
+      <input
+        type="radio"
+        id={`opcion-${id}`}
+        name="opcion"
+        className="radial-input"
+        onClick={handleClick}
+      />
+      <label
+        htmlFor={`opcion-${id}`}
+        className="radial-label pointer inline-block m-5 border-2 border-transparent"
+      >
+        <img src={image} alt={name} />
+      </label>
+    </div>
+  );
+};
