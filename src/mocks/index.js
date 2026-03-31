3fc5b747178182ed7361895a085a86fec665dbc6
Martin <maguera37@gmail.com>
2026-03-31 17:09:36 -0300

    fix: corrected image paths in mock data

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -2,21 +2,21 @@ export const DATA = [
   {
     id: 1,
     name: 'Goku',
-    image: '/images/goku.png',
+    image: 'images/goku.png',
   },
   {
     id: 2,
     name: 'Naruto',
-    image: '/images/naruto.png',
+    image: 'images/naruto.png',
   },
   {
     id: 3,
     name: 'Luffy',
-    image: '/images/luffy.png',
+    image: 'images/luffy.png',
   },
   {
     id: 4,
     name: 'Ichigo',
-    image: '/images/ichigo.png',
+    image: 'images/ichigo.png',
   },
 ];
