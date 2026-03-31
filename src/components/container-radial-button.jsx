fb9831447d8eaa7bc56b926dfb038d59e1c6e042
Martin <maguera37@gmail.com>
2026-03-31 15:27:39 -0300

    feat: added children prop to ContainerRadialButton for rendering additional content

--- src/components/container-radial-button.jsx
+++ src/components/container-radial-button.jsx
@@ -1,10 +1,11 @@
 import { DATA } from '../mocks';
 import { RadialButton } from './radial-button';
 
-export const ContainerRadialButton = () => (
+export const ContainerRadialButton = ({ children }) => (
   <div className="container-radial-button">
     {DATA.map((item) => (
       <RadialButton key={item.id} {...item} />
     ))}
+    <div className="container-radial-children">{children}</div>
   </div>
 );
