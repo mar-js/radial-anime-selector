ab86a047695671873fc1ae75c00df75c0dfeca06
Martin <maguera37@gmail.com>
2026-04-01 15:47:38 -0300

    refactor: simplified App component by removing useEffect and integrating Background component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-import { useEffect } from 'react';
+import { Background } from './components/background';
 import { Container } from './components/container';
 import { ContainerActionsButton } from './components/container-actions-button';
 import { ContainerRadialButton } from './components/container-radial-button';
-import { useData } from './contexts';
 import { DataProvider } from './providers';
 
-export const App = () => {
-  const { data } = useData();
-
-  useEffect(() => {
-    document
-      .getElementsByTagName('main')[0]
-      .style.setProperty('--url-bg', `url(${data.image})`);
-  }, [data]);
-
-  return (
-    <DataProvider>
-      <main data-url-bg={`${data.image}`}>
-        <section>
-          <Container>
-            <ContainerRadialButton>
-              <ContainerActionsButton />
-            </ContainerRadialButton>
-          </Container>
-        </section>
-      </main>
-    </DataProvider>
-  );
-};
+export const App = () => (
+  <DataProvider>
+    <Background>
+      <section>
+        <Container>
+          <ContainerRadialButton>
+            <ContainerActionsButton />
+          </ContainerRadialButton>
+        </Container>
+      </section>
+    </Background>
+  </DataProvider>
+);
