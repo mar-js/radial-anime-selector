b4cc651c4109d17717f57ca8ea93b4b73c9038fb
Martin <maguera37@gmail.com>
2026-04-01 03:46:19 -0300

    feat: updated background image handling to use dynamic URL from data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Container } from './components/container';
 import { ContainerActionsButton } from './components/container-actions-button';
 import { ContainerRadialButton } from './components/container-radial-button';
@@ -7,9 +8,15 @@ import { DataProvider } from './providers';
 export const App = () => {
   const { data } = useData();
 
+  useEffect(() => {
+    document
+      .getElementsByTagName('main')[0]
+      .style.setProperty('--url-bg', `url(${data.image})`);
+  }, [data]);
+
   return (
     <DataProvider>
-      <main data-url-bg={`/public/${data.image}`}>
+      <main data-url-bg={`${data.image}`}>
         <section>
           <Container>
             <ContainerRadialButton>
