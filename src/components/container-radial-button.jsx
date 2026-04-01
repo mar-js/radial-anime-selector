0287739d2257041c92df24cbf53c3d2291f1026e
Martin <maguera37@gmail.com>
2026-04-01 16:26:41 -0300

    fix: refactored ContainerRadialButton to add drag functionality with pointer events

--- src/components/container-radial-button.jsx
+++ src/components/container-radial-button.jsx
@@ -1,11 +1,46 @@
+import { useCallback, useEffect, useRef } from 'react';
 import { DATA } from '../mocks';
 import { RadialButton } from './radial-button';
 
-export const ContainerRadialButton = ({ children }) => (
-  <div className="container-radial-button">
-    {DATA.map((item) => (
-      <RadialButton key={item.id} {...item} />
-    ))}
-    <div className="container-radial-children">{children}</div>
-  </div>
-);
+export const ContainerRadialButton = ({ children }) => {
+  const ref = useRef(null);
+  let offsetX = 0;
+  let offsetY = 0;
+
+  const alMover = useCallback(
+    (e) => {
+      const x = e.clientX - offsetX - 200;
+      const y = e.clientY - offsetY - 200;
+
+      ref.current.style.left = `${x}px`;
+      ref.current.style.top = `${y}px`;
+    },
+    [offsetX, offsetY],
+  );
+
+  useEffect(() => {
+    ref.current.addEventListener('pointerdown', (e) => {
+      const rect = ref.current.getBoundingClientRect();
+      offsetX = e.clientX - rect.left - 200;
+      offsetY = e.clientY - rect.top - 200;
+
+      document.addEventListener('pointermove', alMover);
+      document.addEventListener(
+        'pointerup',
+        () => {
+          document.removeEventListener('pointermove', alMover);
+        },
+        { once: true },
+      );
+    });
+  }, [alMover, offsetX, offsetY]);
+
+  return (
+    <div ref={ref} className="container-radial-button">
+      {DATA.map((item) => (
+        <RadialButton key={item.id} {...item} />
+      ))}
+      <div className="container-radial-children">{children}</div>
+    </div>
+  );
+};
