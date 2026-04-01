2648385a442e80b1d21b30f71a7fce7fc1252105
Martin <maguera37@gmail.com>
2026-04-01 15:47:31 -0300

    feat: added Background component to dynamically set background image

---
+++ src/components/background.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useData } from '../contexts';
+
+export const Background = ({ children }) => {
+  const { data } = useData();
+
+  useEffect(() => {
+    document
+      .getElementsByTagName('main')[0]
+      .style.setProperty('--url-bg', `url(${data.image})`);
+  }, [data]);
+
+  return <main data-url-bg={`${data.image}`}>{children}</main>;
+};
